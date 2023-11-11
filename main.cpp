3d9b411b316124fca772db5dcafe39ca37b71687
Daniel Adámek <daniel.adamek123@icloud.dom>
2023-11-11 22:37:09 +0100

    example phone loader

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "src/loaders/CPhoneLoaderFactory.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<CContact> contacts;
+    try {
+        std::string path = "../data/phones.csv";
+        auto loader = CPhoneLoaderFactory::createLoader(path);
+        loader->load();
+        contacts = loader->export_contacts();
+
+    } catch (const UnknownFileTypeException& e) {
+        std::cerr << "Error: " <<  e.what() << std::endl;
+    }
+    std::cout << contacts.size() << std::endl;
     return 0;
 }
