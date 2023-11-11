f05721232cc2b86c91a61fab383782459ea4863f
Daniel Adámek <daniel.adamek123@icloud.dom>
2023-11-11 23:39:26 +0100

    string normalize, string_to_t9

--- src/CContactManager.cpp
+++ src/CContactManager.cpp
@@ -7,9 +7,53 @@
 #include "CContactManager.h"
 
 void CContactManager::loadContacts(const std::vector<CContact> &contacts) {
-    m_Contacts = contacts;
+    for (const auto& contact : contacts) {
+        m_Contacts.push_back(contact);
+    }
 }
 
-std::vector<CContact> CContactManager::search(const std::string &query) {
-    return std::vector<CContact>();
+std::vector<CContact> CContactManager::search(std::string_view query) {
+    std::vector<CContact> result;
+
+    // Analyze query is number or name
+    bool isNumber = true;
+    for (const auto& c : query) {
+        if (!std::isdigit(c)) {
+            isNumber = false;
+            break;
+        }
+    }
+    if (!isNumber) {
+        // Find contacts by name
+        for (const auto& contact : m_Contacts) {
+            if (contact.getName().find(query) != std::string::npos) {
+                result.push_back(contact);
+            }
+        }
+        return result;
+    }
+
+    // Find contacts by phone
+    for (const auto& contact : m_Contacts) {
+        if (contact.second.getPhone().find(query) != std::string::npos) {
+            result.push_back(contact.second);
+        }
+    }
+
+
+    return result;
+}
+
+std::string CContactManager::normalize(std::string_view str) {
+    std::string result;
+    for (const char& c : str)
+        if (std::isalpha(c)) result += std::tolower(c);
+    return result;
+}
+
+std::string CContactManager::string_to_t9_number(std::string_view input) {
+    std::string output;
+    for (const char& c : input)
+        output += (std::isalpha(c)) ? t9_map[c] : c;
+    return output;
 }
--- src/CContactManager.h
+++ src/CContactManager.h
@@ -9,13 +9,43 @@
 
 
 #include <vector>
+#include <unordered_map>
+#include <map>
 #include "CContact.h"
 
 class CContactManager {
 private:
-    std::vector<CContact> m_Contacts;
+    std::unordered_map<char, char> t9_map = {
+        {'a', '2'}, {'b', '2'}, {'c', '2'},
+        {'d', '3'}, {'e', '3'}, {'f', '3'},
+        {'g', '4'}, {'h', '4'}, {'i', '4'},
+        {'j', '5'}, {'k', '5'}, {'l', '5'},
+        {'m', '6'}, {'n', '6'}, {'o', '6'},
+        {'p', '7'}, {'q', '7'}, {'r', '7'}, {'s', '7'},
+        {'t', '8'}, {'u', '8'}, {'v', '8'},
+        {'w', '9'}, {'x', '9'}, {'y', '9'}, {'z', '9'}
+    };
+    std::map<std::string, CContact> m_Contacts;
 
+    /**
+     * Normalize string
+     * @param str
+     * @return
+     */
+    static std::string normalize(std::string_view str);
+
+    /**
+     * Normalize string
+     * @param str
+     * @return
+     */
+    std::string string_to_t9_number(std::string_view str);
 public:
+    /**
+     * Constructor
+     */
+    CContactManager() = default;
+
     /**
      * Load contacts from vector
      * @param contacts
@@ -27,7 +57,7 @@ public:
      * @param query
      * @return
      */
-    std::vector<CContact> search(const std::string& query);
+    std::vector<CContact> search(std::string_view query);
 };
 
 
