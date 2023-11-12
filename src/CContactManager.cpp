d39f2451fb0b0776d8e749b769496fc1e5a199c9
Daniel Adámek <daniel.adamek123@icloud.dom>
2023-11-12 14:49:37 +0100

    CContact as shared_ptr

--- src/CContactManager.cpp
+++ src/CContactManager.cpp
@@ -6,14 +6,24 @@
 
 #include "CContactManager.h"
 
-void CContactManager::loadContacts(const std::vector<CContact> &contacts) {
-    for (const auto& contact : contacts) {
-        m_Contacts.push_back(contact);
+
+CContactManager::CContactManager() {
+    m_SuffixTree = std::make_unique<CSuffixTree<std::shared_ptr<CContact>>>();
+}
+
+void CContactManager::loadContacts(const std::vector<CContact>& contacts) {
+    m_Contacts.clear();
+    // m_SuffixTree->clear();
+
+    for (const CContact& contact : contacts) {
+        auto new_contact = std::make_shared<CContact>(contact);
+        m_Contacts.push_back(new_contact);
+        m_SuffixTree->insert(contact.getT9Representation(), new_contact);
     }
 }
 
-std::vector<CContact> CContactManager::search(std::string_view query) {
-    std::vector<CContact> result;
+std::vector<std::shared_ptr<CContact>> CContactManager::search(std::string_view query) {
+    std::vector<std::shared_ptr<CContact>> result;
 
     // Analyze query is number or name
     bool isNumber = true;
@@ -26,7 +36,7 @@ std::vector<CContact> CContactManager::search(std::string_view query) {
     if (!isNumber) {
         // Find contacts by name
         for (const auto& contact : m_Contacts) {
-            if (contact.getName().find(query) != std::string::npos) {
+            if (contact->getName().find(query) != std::string::npos) {
                 result.push_back(contact);
             }
         }
@@ -35,25 +45,11 @@ std::vector<CContact> CContactManager::search(std::string_view query) {
 
     // Find contacts by phone
     for (const auto& contact : m_Contacts) {
-        if (contact.second.getPhone().find(query) != std::string::npos) {
-            result.push_back(contact.second);
+        if (contact->getPhone().find(query) != std::string::npos) {
+            result.push_back(contact);
         }
     }
 
 
     return result;
-}
-
-std::string CContactManager::normalize(std::string_view str) {
-    std::string result;
-    for (const char& c : str)
-        if (std::isalpha(c)) result += std::tolower(c);
-    return result;
-}
-
-std::string CContactManager::string_to_t9_number(std::string_view input) {
-    std::string output;
-    for (const char& c : input)
-        output += (std::isalpha(c)) ? t9_map[c] : c;
-    return output;
 }
--- src/CContactManager.h
+++ src/CContactManager.h
@@ -12,44 +12,20 @@
 #include <unordered_map>
 #include <map>
 #include "CContact.h"
+#include "data_structures/CSuffixTree.h"
 
 class CContactManager {
 private:
-    std::unordered_map<char, char> t9_map = {
-        {'a', '2'}, {'b', '2'}, {'c', '2'},
-        {'d', '3'}, {'e', '3'}, {'f', '3'},
-        {'g', '4'}, {'h', '4'}, {'i', '4'},
-        {'j', '5'}, {'k', '5'}, {'l', '5'},
-        {'m', '6'}, {'n', '6'}, {'o', '6'},
-        {'p', '7'}, {'q', '7'}, {'r', '7'}, {'s', '7'},
-        {'t', '8'}, {'u', '8'}, {'v', '8'},
-        {'w', '9'}, {'x', '9'}, {'y', '9'}, {'z', '9'}
-    };
-    std::map<std::string, CContact> m_Contacts;
-
-    /**
-     * Normalize string
-     * @param str
-     * @return
-     */
-    static std::string normalize(std::string_view str);
-
-    /**
-     * Normalize string to t9-like format
-     * @param str
-     * @return
-     */
-    std::string string_to_t9_number(std::string_view str);
+    std::vector<std::shared_ptr<CContact>> m_Contacts;
 public:
+    std::unique_ptr<CSuffixTree<std::shared_ptr<CContact>>> m_SuffixTree;
+
     /**
      * Constructor
      */
-    CContactManager() = default;
+    CContactManager();
 
-    /**
-     * Load contacts from vector
-     * @param contacts
-     */
+
     void loadContacts(const std::vector<CContact>& contacts);
 
     /**
@@ -57,7 +33,7 @@ public:
      * @param query
      * @return
      */
-    std::vector<CContact> search(std::string_view query);
+    std::vector<std::shared_ptr<CContact>> search(std::string_view query);
 };
 
 
