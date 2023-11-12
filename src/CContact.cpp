6aa62b6cb221dad720375ef1afc18ca4190bcf7f
Daniel Adámek <daniel.adamek123@icloud.dom>
2023-11-12 14:50:03 +0100

    t9 representation

--- src/CContact.cpp
+++ src/CContact.cpp
@@ -6,7 +6,9 @@
 
 #include "CContact.h"
 
-CContact::CContact(std::string name, std::string phone): m_Name(std::move(name)), m_Phone(std::move(phone)) {}
+ CContact::CContact(std::string name, std::string phone): m_Name(std::move(name)), m_Phone(std::move(phone)) {
+    m_T9_representation = string_to_t9_number(normalize(m_Name));
+}
 
 const std::string &CContact::getName() const {
     return m_Name;
@@ -14,4 +16,21 @@ const std::string &CContact::getName() const {
 
 const std::string &CContact::getPhone() const {
     return m_Phone;
+}
+std::string CContact::normalize(std::string_view str) {
+    std::string result;
+    for (const char& c : str)
+        if (std::isalpha(c)) result += std::tolower(c);
+    return result;
+}
+
+std::string CContact::string_to_t9_number(std::string_view input) {
+    std::string output;
+    for (const char& c : input)
+        output += (std::isalpha(c)) ? t9_map[c] : c;
+    return output;
+}
+
+const std::string &CContact::getT9Representation() const {
+    return m_T9_representation;
 }
